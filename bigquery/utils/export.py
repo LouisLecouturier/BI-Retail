493fa77f65f99c103342dd7e3b2fc4167ef82748
Antoine Maes <92756820+antoine-maes@users.noreply.github.com>
2024-11-27 22:23:56 +0100

    Update export.py

--- bigquery/utils/export.py
+++ bigquery/utils/export.py
@@ -2,7 +2,7 @@ from utils.create import create_dataset
 from pandas_gbq import to_gbq
 
 
-def export_table(client, df, project_id, dataset_name, table_name):
+def export_table(client, df, dataset_name, table_name, project_id="power-bi-m2"):
     create_dataset(client, dataset_name, project_id)
     return to_gbq(
         dataframe=df,
